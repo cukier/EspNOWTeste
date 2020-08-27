13dac7a4bf0271ea3ce0e98ffd34ed7cf1c3e66f
cukier <mauriciocukier@gmail.com>
2020-08-27 14:05:14 -0300

    limpeza
    Signed-off-by: cukier <mauriciocukier@gmail.com>

--- main/espnow.c
+++ main/espnow.c
@@ -25,48 +25,25 @@
 #define CONFIG_ESPNOW_CHANNEL 		1
 #define CONFIG_ESPNOW_PMK 			"pmk1234567890123"
 
-//typedef enum {
-//	CMD_STP, CMD_UP, CMD_DOWN, CMD_EM
-//} espnow_cmd_t;
-//
-//typedef struct {
-//	espnow_cmd_t cmd;
-//	uint64_t pos;
-//} espnow_pkg_t;
-
-//typedef struct {
-//	uint8_t mac_addr[ESP_NOW_ETH_ALEN];
-//	esp_now_send_status_t status;
-//} espnow_event_send_t;
-
 typedef struct {
 	uint8_t mac_addr[ESP_NOW_ETH_ALEN];
 	uint8_t *data;
 	int data_len;
-//	int has_data;
 } espnow_event_recv_t;
 
-//static xQueueHandle espnow_queue;
 static const char *TAG = "espnow_cuki";
 static espnow_event_recv_t evt;
-//static espnow_pkg_t *pkg = NULL;
 static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 		0xFF };
 
 static void espnow_recv_cb(const uint8_t *mac_addr, const uint8_t *data,
 		int len) {
-//	static espnow_event_recv_t evt;
-//	int cont;
 
 	if (mac_addr == NULL || data == NULL || len <= 0) {
 		ESP_LOGE(TAG, "Receive cb arg error");
 		return;
 	} else {
 		ESP_LOGI(TAG, "Receive %u", len);
-
-//		for (cont = 0; cont < len; ++cont) {
-//			ESP_LOGI(TAG, "%03u : %03u", cont, data[cont]);
-//		}
 	}
 
 	if (evt.data_len > 0) {
@@ -85,11 +62,6 @@ static void espnow_recv_cb(const uint8_t *mac_addr, const uint8_t *data,
 	memcpy(evt.mac_addr, mac_addr, ESP_NOW_ETH_ALEN);
 	memcpy(evt.data, data, len);
 	evt.data_len = len;
-
-//	if (xQueueSend(espnow_queue, &evt, portMAX_DELAY) != pdTRUE) {
-//		ESP_LOGW(TAG, "Send receive queue fail");
-//		free(evt.data);
-//	}
 }
 
 int wifi_init() {
@@ -109,15 +81,7 @@ esp_err_t espnow_init() {
 
 	wifi_init();
 
-//	espnow_queue = xQueueCreate(ESPNOW_QUEUE_SIZE, sizeof(uint8_t));
-
-//	if (espnow_queue == NULL) {
-//		ESP_LOGE(TAG, "Create mutex fail");
-//		return ESP_FAIL;
-//	}
-
 	ESP_ERROR_CHECK(esp_now_init());
-//	ESP_ERROR_CHECK(esp_now_register_send_cb(example_espnow_send_cb));
 	ESP_ERROR_CHECK(esp_now_register_recv_cb(espnow_recv_cb));
 	ESP_ERROR_CHECK(esp_now_set_pmk((uint8_t* )CONFIG_ESPNOW_PMK));
 
@@ -125,7 +89,6 @@ esp_err_t espnow_init() {
 
 	if (peer == NULL) {
 		ESP_LOGE(TAG, "Malloc peer information fail");
-//		vSemaphoreDelete(espnow_queue);
 		esp_now_deinit();
 		return ESP_FAIL;
 	}
@@ -144,7 +107,6 @@ esp_err_t espnow_init() {
 void espnow_run() {
 	int cont, send_cont = 0;
 	uint8_t data[ESPNOW_QUEUE_SIZE] = { 0 };
-//	static espnow_event_recv_t evt;
 
 	ESP_ERROR_CHECK(espnow_init());
 	ESP_LOGI(TAG, "init ok");
@@ -156,37 +118,25 @@ void espnow_run() {
 	while (1) {
 		vTaskDelay(100 / portTICK_RATE_MS);
 		++send_cont;
-//		for (cont = 0; cont < ESPNOW_QUEUE_SIZE; ++cont) {
-//			data[cont] = cont + 1;
-//		}
+
 		if (send_cont > 100) {
 			send_cont = 0;
 			esp_err_t err = esp_now_send(broadcast_mac, data,
 			ESPNOW_QUEUE_SIZE);
-//		ESP_LOGI(TAG, "enviado: 0x%x", err);
 
 			if (esp_now_send(broadcast_mac, data, ESPNOW_QUEUE_SIZE) != ESP_OK) {
 				ESP_LOGE(TAG, "Send error");
-//		} else {
-//			ESP_LOGI(TAG, "send data ok");
 			}
 		}
 
-//		for (cont = 0; cont < ESPNOW_QUEUE_SIZE; ++cont) {
-//			data[cont] = 0;
-//		}
-
-//		if (xQueueReceive(espnow_queue, &evt, portMAX_DELAY) == pdTRUE) {
 		if (evt.data_len > 0) {
 			ESP_LOGI(TAG, "Recebido: %u de "MACSTR"", evt.data_len,
 					MAC2STR(evt.mac_addr));
 
 			for (cont = 0; cont < evt.data_len; ++cont) {
-//			for (cont = 0; cont < ESPNOW_QUEUE_SIZE; ++cont) {
 				ESP_LOGI(TAG, "%5d-0x%x ", cont, evt.data[cont]);
 			}
 
-//			if (evt.data_len > 0)
 			free(evt.data);
 			evt.data = NULL;
 			evt.data_len = 0;
